a31b39c4ca053a781fa9536181c0a4278f7bc102
sindi702 <hamatajsindi@gmail.com>
2023-09-21 03:36:54 -0700

    test

--- models/city.py
+++ models/city.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python3
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from sqlalchemy import ForeignKey
+from models.base_model import BaseModel, Base
+0
 """ City Module for HBNB project """
-from models.base_model import BaseModel
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    state_id = Column(String(60), ForeignKey(
+        'states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship('Place', backref='cities',
+                          cascade='delete')#!/usr/bin/python3
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from sqlalchemy import ForeignKey
+from models.base_model import BaseModel, Base
+0
+""" City Module for HBNB project """
+
+
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+    state_id = Column(String(60), ForeignKey(
+        'states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship('Place', backref='cities',
+                          cascade='delete')
